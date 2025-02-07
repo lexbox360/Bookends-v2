7c31058fd607c04720acb6aff412056b1b0e336d
lexrayne <30938032+lexrayne@users.noreply.github.com>
2025-02-07 19:52:43 -0400

    Added mongoose

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
+
+connectDB(); // Connect to MongoDB
 
 const app = express();
 
