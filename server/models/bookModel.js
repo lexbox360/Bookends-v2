44890612e4de1b4bc77d7512bce48df232a2c265
lexrayne <30938032+lexrayne@users.noreply.github.com>
2025-02-08 03:46:33 -0400

    Initial commit

---
+++ server/models/bookModel.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+    },
+    comment: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
+const bookSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    cover: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    numReviews: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+}, {
+    timestamps: true,
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+export default Book;
