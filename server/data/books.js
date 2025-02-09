8bb2c8bdc4fb2b66a9d21c71a7580c22ede41ebe
lexrayne <30938032+lexrayne@users.noreply.github.com>
2025-02-08 21:01:33 -0400

    Initial commit

---
+++ server/data/books.js
@@ -0,0 +1,70 @@
+const books = [
+    {
+        name: "Gideon the Ninth",
+        author: "Tamsyn Muir",
+        cover: "/images/gideon-the-ninth.jpg",
+        description: "The Emperor needs necromancers. The Ninth Necromancer needs a swordswoman. Gideon has a sword, some dirty magazines, and no more time for undead bullshit. Brought up by unfriendly, ossifying nuns, ancient retainers, and countless skeletons, Gideon is ready to abandon a life of servitude and an afterlife as a reanimated corpse. She packs up her sword, her shoes, and her dirty magazines, and prepares to launch her daring escape. But her childhood nemesis won't set her free without a service. Harrowhark Nonagesimus, Reverend Daughter of the Ninth House and bone witch extraordinaire, has been summoned into action. The Emperor has invited the heirs to each of his loyal Houses to a deadly trial of wits and skill. If Harrowhark succeeds she will become an immortal, all-powerful servant of the Resurrection, but no necromancer can ascend without their cavalier. Without Gideon's sword, Harrow will fail, and the Ninth House will die. Of course, some things are better left dead.",
+        genreTags: ["Fantasy", "Science Fiction", "Queer", "Fiction", "LGBT", "Horror", "Lesbian", "Adult", "Mystery", "Science Fiction Fantasy"],
+        rating: 4.2,
+        numReviews: 0,
+        price: 25.99,
+        countInStock: 10
+    },
+    {
+        name: "Harrow the Ninth",
+        author: "Tamsyn Muir",
+        cover: "/images/harrow-the-ninth.jpg",
+        description: "Harrow the Ninth, the sequel to Gideon the Ninth, turns a galaxy inside out as one necromancer struggles to survive the wreckage of herself aboard the Emperor's haunted space station. She answered the Emperor's call. She arrived with her arts, her wits, and her only friend. In victory, her world has turned to ash. After rocking the cosmos with her deathly debut, Tamsyn Muir continues the story of the penumbral Ninth House in Harrow the Ninth, a mind-twisting puzzle box of mystery, murder, magic, and mayhem. Nothing is as it seems in the halls of the Emperor, and the fate of the galaxy rests on one woman's shoulders. Harrowhark Nonagesimus, last necromancer of the Ninth House, has been drafted by her Emperor to fight an unwinnable war. Side-by-side with a detested rival, Harrow must perfect her skills and become an angel of undeath — but her health is failing, her sword makes her nauseous, and even her mind is threatening to betray her. Sealed in the gothic gloom of the Emperor's Mithraeum with three unfriendly teachers, hunted by the mad ghost of a murdered planet, Harrow must confront two unwelcome questions: is somebody trying to kill her? And if they succeeded, would the universe be better off?",
+        genreTags: ["Fantasy", "Science Fiction", "Queer", "Fiction", "LGBT", "Horror", "Lesbian", "Adult", "Mystery", "Science Fiction Fantasy"],
+        rating: 4.3,
+        numReviews: 0,
+        price: 25.99,
+        countInStock: 9
+    },
+    {
+        name: "Nona the Ninth",
+        author: "Tamsyn Muir",
+        cover: "/images/nona-the-ninth.jpg",
+        description: "Her city is under siege. The zombies are coming back. And all Nona wants is a birthday party. In many ways, Nona is like other people. She lives with her family, has a job at her local school, and loves walks on the beach and meeting new dogs. But Nona's not like other people. Six months ago she woke up in a stranger's body, and she's afraid she might have to give it back. The whole city is falling to pieces. A monstrous blue sphere hangs on the horizon, ready to tear the planet apart. Blood of Eden forces have surrounded the last Cohort facility and wait for the Emperor Undying to come calling. Their leaders want Nona to be the weapon that will save them from the Nine Houses. Nona would prefer to live an ordinary life with the people she loves, with Pyrrha and Camilla and Palamedes, but she also knows that nothing lasts forever. And each night, Nona dreams of a woman with a skull-painted face...",
+        genreTags: ["Fantasy", "Science Fiction", "Queer", "Fiction", "LGBT", "Horror", "Lesbian", "Adult", "Science Fiction Fantasy"],
+        rating: 4.3,
+        numReviews: 0,
+        price: 25.99,
+        countInStock: 6
+    },
+    {
+        name: "A Memory Called Empire",
+        author: "Arkady Martine",
+        cover: "/images/a-memory-called-empire.jpg",
+        description: "Ambassador Mahit Dzmare arrives in the center of the multi-system Teixcalaanli Empire only to discover that her predecessor, the previous ambassador from their small but fiercely independent mining Station, has died. But no one will admit that his death wasn't an accident—or that Mahit might be next to die, during a time of political instability in the highest echelons of the imperial court. Now, Mahit must discover who is behind the murder, rescue herself, and save her Station from Teixcalaan's unceasing expansion—all while navigating an alien culture that is all too seductive, engaging in intrigues of her own, and hiding a deadly technological secret—one that might spell the end of her Station and her way of life—or rescue it from annihilation.",
+        genreTags: ["Science Fiction", "Fiction", "Space Opera", "Queer", "LGBT", "Space", "Adult", "Lesbian"],
+        rating: 4.1,
+        numReviews: 0,
+        price: 25.99,
+        countInStock: 10
+    },
+    {
+        name: "A Desolation Called Peace",
+        author: "Arkady Martine",
+        cover: "/images/a-desolation-called-peace.jpg",
+        description: "A Desolation Called Peace is the spectacular space opera sequel to Arkady Martine's genre-reinventing, Hugo Award-winning debut, A Memory Called Empire. An alien armada lurks on the edges of Teixcalaanli space. No one can communicate with it, no one can destroy it, and Fleet Captain Nine Hibiscus is running out of options. In a desperate attempt at diplomacy with the mysterious invaders, the fleet captain has sent for a diplomatic envoy. Now Mahit Dzmare and Three Seagrass—still reeling from the recent upheaval in the Empire—face the impossible task of trying to communicate with a hostile entity. Their failure will guarantee millions of deaths in an endless war. Their success might prevent Teixcalaan’s destruction—and allow the empire to continue its rapacious expansion. Or it might create something far stranger . . .",
+        genreTags: ["Science Fiction", "Fiction", "Space Opera", "Queer", "LGBT", "Space", "Adult", "Lesbian"],
+        rating: 4.3,
+        numReviews: 0,
+        price: 25.99,
+        countInStock: 9
+    },
+    {
+        name: "This Is How You Lose the Time War",
+        author: "Amal El-Mohtar, Max Gladstone",
+        cover: "/images/this-is-how-you-lose-the-time-war.jpg",
+        description: "Among the ashes of a dying world, an agent of the Commandant finds a letter. It reads: Burn before reading. Thus begins an unlikely correspondence between two rival agents hellbent on securing the best possible future for their warring factions. Now, what began as a taunt, a battlefield boast, grows into something more. Something epic. Something romantic. Something that could change the past and the future. Except the discovery of their bond would mean death for each of them. There's still a war going on, after all. And someone has to win that war.",
+        genreTags: ["Science Fiction", "Romance", "Fiction", "Fantasy", "LGBT", "Queer", "Time Travel", "Lesbian"],
+        rating: 3.9,
+        numReviews: 0,
+        price: 19.99,
+        countInStock: 7
+    }
+];
+
+export default books;
